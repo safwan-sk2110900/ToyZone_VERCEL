cc7e870438adc667a090eec8715caa436796bfcf
Safwan Abdurahiman Kavil <157449123+safwan-sk2110900@users.noreply.github.com>
2024-05-13 02:56:45 +0300

    changes for files

--- Project_Phase2_VERCEL/web-app/app/summary/NeverPurchased/page.jsx
+++ Project_Phase2_VERCEL/web-app/app/summary/NeverPurchased/page.jsx
@@ -1,11 +1,50 @@
-import React from 'react'
-import toysRepo from '../../repo/toysrepo';
-import Never from '../../components/NeverPurchased/Never'
-export default async function page() {
-    const nvrItems = await toysRepo.getNeverPurchased()
-  return (
-    <Never items={nvrItems}></Never>
-  )
-}
+'use client'
+import React, { useState, useEffect } from 'react';
+import styles from '../../page.module.css';
 
+export default function Page() {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        async function fetchProducts() {
+            try {
+                const response = await fetch('/api/summary/neverpurchased', { method: 'GET' });
+                const data = await response.json();
+                setProducts(data);
+            } catch (error) {
+                console.error('Failed to fetch products:', error);
+            }
+        }
+
+        fetchProducts();
+    }, []);
+
+    return (
+        <div className={styles.tableContainer}>
+            <table className={styles.table}>
+                <thead>
+                    <tr>
+                        <th colSpan="3" className={styles.tableSellerHeader}>
+                            Items Never Purchased
+                        </th>
+                    </tr>
+                    <tr>
+                        <th className={styles.tableSellerCell}>Product Name</th>
+                        <th className={styles.tableSellerCell}>Image</th>
+                        <th className={styles.tableSellerCell}>Seller</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {products.map(product => (
+                        <tr className={styles.tableSellerRow}>
+                            <td className={styles.tableSellerCell}>{product.name}</td>
+                            <td className={styles.tableSellerCell}><img src={`../../${product.image}` } alt={product.name} width="60%" height="25%" /></td>
+                            <td className={styles.tableSellerCell}>{product.seller}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
+}
 
--- Project_Phase2_VERCEL/web-app/app/summary/PerProduct/page.jsx
+++ Project_Phase2_VERCEL/web-app/app/summary/PerProduct/page.jsx
@@ -1,17 +1,69 @@
-import Product from "@/app/components/PerProduct/Product"
-import toysRepo from '../../repo/toysrepo';
-import React from 'react'
-import NavBar from '@/app/common/NavBar'
-import Footer from '@/app/common/Footer'
+'use client'
+import React, { useState, useEffect } from 'react';
+import styles from '../../page.module.css';
 export default async function Home() {
   
-   
-    const perProduct = await toysRepo.purchasePerProduct();
-    const items = await toysRepo.getItems();
+  const [products, setProducts] = useState([]);
+  const [items, setItems] = useState([]);
+  const [filteredProduct] = useState([]);
+
+  useEffect(() => {
+      async function fetchProducts() {
+          try {
+              const response = await fetch('/api/summary/perproduct', { method: 'GET' });
+              const response1 = await fetch('/api/items', { method: 'GET' });
+              const data = await response.json();
+              const data1 = await response1.json();
+              setProducts(data);
+              setItems(data1)
+              items.forEach(item=>{
+                products.forEach(product=>{
+                  if(item.id==product.productid){
+                    product.productid=item.name
+                    filteredProduct.push(product)
+                  }
+                })
+              })
+          } catch (error) {
+              console.error('Failed to fetch data:', error);
+          }
+      }
+
+      fetchProducts();
+
+  }, []);
+
+
+  
+  
     
  
-  return (
-         <Product perProduct={perProduct} items={items}></Product>
-  )
+    return (
+      <div className={styles.tableContainer}>
+              <table className={styles.table}>
+                  <thead>
+                      <tr>
+                          <th colSpan="2" className={styles.tableSellerHeader}>
+                              Purchases Per Product
+                          </th>
+                      </tr>
+                      <tr>
+                          <th className={styles.tableSellerCell}>Product Name</th>
+                          <th className={styles.tableSellerCell}>Total Quantity</th>
+                      </tr>
+                  </thead>
+                  <tbody>
+                      {filteredProduct.map(product => (
+                          <tr className={styles.tableSellerRow}>
+                              <td className={styles.tableSellerCell}>{product.productid}</td>
+                              <td className={styles.tableSellerCell}>{product._sum.qty}</td>
+                          </tr>
+                      ))}
+                  </tbody>
+              </table>
+          </div>
+  
+    )
+  
 }
 
